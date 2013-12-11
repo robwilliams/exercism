29abbd0841e7bf9e271c5ace3696748e0e9f6968
Robert Williams <rob@r-williams.com>
2013-12-11 00:03:08 +0000

    Another way but I don't think it reads any better.

--- ruby/leap/year.rb
+++ ruby/leap/year.rb
@@ -1,16 +1,17 @@
 class Year < Struct.new(:year)
 
   def leap?
-    if    divides_by?(400) then true
-    elsif divides_by?(100) then false
-    elsif divides_by?(4)   then true
+    case year
+    when divides_by[400] then true
+    when divides_by[100] then false
+    when divides_by[4]   then true
     else
       false
     end
   end
 
   private
-  def divides_by?(num)
-    year % num == 0
+  def divides_by
+    -> by, year { year % by == 0 }.curry
   end
 end
